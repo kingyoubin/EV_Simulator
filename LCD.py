7cceead49ef3e8945a3739ea262ce6600b6f52f4
Yubin Kim <83595465+kingyoubin@users.noreply.github.com>
2024-09-09 07:51:55 +0900

    Update LCD.py

--- LCD.py
+++ LCD.py
@@ -3,17 +3,18 @@ import RPi_I2C_driver  # assuming the lcd driver is imported here
 
 # ChargeHandler 클래스 선언
 class LCDHandler:
+
     def __init__(self, lcd_address=0x27):
         # LCD 객체 초기화
         self.lcd = RPi_I2C_driver.lcd(lcd_address)
 
     # 비동기 chargePercent 메서드
     async def chargePercent(self):
-        self.lcd.cursor()
+        self.lcd.cursor()  # 커서 활성화
 
         for i in range(0, 100):
             self.lcd.print("Charging")
-            self.lcd.setcursor(2, 1)
+            self.lcd.setCursor(2, 1)  # setcursor -> setCursor로 수정
             self.lcd.print(f"percent : {i}%")
             await asyncio.sleep(0.5)  # 비동기 처리
             self.lcd.clear()
