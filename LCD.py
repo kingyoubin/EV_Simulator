91c9b3e98eafca358a35bfbf6ab7fb6520f5a399
Yubin Kim <83595465+kingyoubin@users.noreply.github.com>
2024-09-09 09:18:00 +0900

    Update LCD.py

--- LCD.py
+++ LCD.py
@@ -12,10 +12,10 @@ class LCDHandler:
     def chargePercent(self):
         self.lcd.cursor()  # 커서 활성화
 
-        for i in range(0, 101):
+        for i in range(0, 100):
             self.lcd.lcd_display_string("Charging", 1)  # 첫 번째 줄에 "Charging" 출력
             self.lcd.lcd_display_string_pos(f"percent : {i}%", 2, 1)  # 두 번째 줄 1번째 위치에 퍼센트 출력
-            time.sleep(1)  # 스레드에서 대기 시간을 사용
+            time.sleep(0.5)  # 스레드에서 대기 시간을 사용
         
         self.lcd.lcd_display_string("Charging Finish", 1)  
 
